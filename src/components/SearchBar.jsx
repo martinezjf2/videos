2d285db0df72a9192599b27fc9485c3bae89fb83
Jeffrey Martinez <martinezjf2@gmail.com>
2021-03-12 22:37:49 -0500

    added onChange event

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,14 +3,22 @@ import React from "react";
 export default class SearchBar extends React.Component {
     state = { term: '' };
 
+    onInputChange = (event) => {
+        this.setState({term: event.target.value})
+    };
+
     render() {
+
         return (
             <div className="ui segment search-bar">
                 <form className="ui form">
                     <div className="field">
                         <label>Video Search</label>
-                        <input type="text" value={this.state.term} id="Submit"/>
-                        
+                        <input 
+                            type="text" 
+                            value={this.state.term} 
+                            onChange={this.onInputChange}
+                        />
                     </div>
                 </form>
             </div>
