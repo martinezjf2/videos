2ebe3c8a51f7783139badd54cfc42be17582e487
Jeffrey Martinez <martinezjf2@gmail.com>
2021-03-12 22:40:05 -0500

    created the onFormSubmit handler

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -7,11 +7,16 @@ export default class SearchBar extends React.Component {
         this.setState({term: event.target.value})
     };
 
+    onFormSubmit = (event) => {
+        event.preventDefault();
+        // TODO: Make sure we call callback from parent component
+    }
+
     render() {
 
         return (
             <div className="ui segment search-bar">
-                <form className="ui form">
+                <form onSubmit={this.onFormSubmit} className="ui form">
                     <div className="field">
                         <label>Video Search</label>
                         <input 
