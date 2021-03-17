4289c2a90bc8c3909ae854ae6009f15c08a13ee6
Jeffrey Martinez <martinezjf2@gmail.com>
2021-03-16 22:12:21 -0400

    added params to the axios request

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -3,5 +3,10 @@ import axios from "axios";
 const KEY = 'AIzaSyBjynl2RBOYcHoyAH_Om2WjkAuP-Vmz20A';
 
 export default axios.create({
-    baseURL: "https://www.googleapis.com/youtube/v3"
+    baseURL: "https://www.googleapis.com/youtube/v3",
+    params: {
+        part: 'snippet',
+        maxResults: 5,
+        key: KEY
+    }
 })
