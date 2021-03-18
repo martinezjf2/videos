a97ca07aa2ff5647cdd64ef1fab194dd799b985e
Jeffrey Martinez <martinezjf2@gmail.com>
2021-03-17 20:13:44 -0400

    imported youtube from apis to get the search

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import SearchBar from "./components/SearchBar"
+import youtube from './apis/youtube'
 
 export default class App extends React.Component {
 
    onTermSubmit = (term) => {
-
+    youtube.get('/search', {
+        params: {
+            q: term
+        }
+    })
    }
     
     render() {
