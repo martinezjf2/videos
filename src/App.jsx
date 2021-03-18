2c2ccf0417f6eadabe0cef59c43cc7d6485f874d
Jeffrey Martinez <martinezjf2@gmail.com>
2021-03-17 20:08:29 -0400

    added onTermSubmit callback function as a prop

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import SearchBar from "./components/SearchBar"
 
 export default class App extends React.Component {
 
-   
+   onTermSubmit = (term) => {
+
+   }
     
     render() {
         return (
             <div className="ui container">
-                <h1><SearchBar/></h1>
+                <h1><SearchBar onFormSubmit={this.onTermSubmit}/></h1>
             </div>
         )
     }
