90f1720c91d59e4d289e724978c53fe86762de5e
Jeffrey Martinez <martinezjf2@gmail.com>
2021-03-17 20:21:08 -0400

    added state for videos to equal an array.

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,22 @@ import SearchBar from "./components/SearchBar"
 import youtube from './apis/youtube'
 
 export default class App extends React.Component {
-
-   onTermSubmit = (term) => {
-    youtube.get('/search', {
+    state = { videos: [] }
+   onTermSubmit = async term => {
+    const response = await youtube.get('/search', {
         params: {
             q: term
         }
-    })
+    });
+    console.log(response.data.items)
+    this.setState({ videos: response.data.items })
    }
     
     render() {
         return (
             <div className="ui container">
                 <h1><SearchBar onFormSubmit={this.onTermSubmit}/></h1>
+                I have {this.state.videos.length} videos.
             </div>
         )
     }
