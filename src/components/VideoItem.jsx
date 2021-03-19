daf692d9205c008eac6ac194b4417b9c871a7be9
Jeffrey Martinez <martinezjf2@gmail.com>
2021-03-18 22:07:19 -0400

    added semantic to adjust design

--- src/components/VideoItem.jsx
+++ src/components/VideoItem.jsx
@@ -1,10 +1,16 @@
+// eslint-disable-next-line
+
 import React from "react"
 
 const VideoItem = ({ video }) => {
     return (
-        <div>
-            <img src={video.snippet.thumbnails.medium.url} />
-            {video.snippet.title}
+        <div className="item">
+                <img className="ui image" src={video.snippet.thumbnails.medium.url} alt="Video" />
+            <div className="content">
+                <div className="header">
+                    {video.snippet.title}
+                </div>
+            </div>
         </div>
     )
 };
