cc384ab826f49cf431dde345225e9136e4fa19c7
Jeffrey Martinez <martinezjf2@gmail.com>
2021-04-07 19:57:14 -0400

    fixed the video.snippet.title

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -13,7 +13,7 @@ const VideoDetail = ({video}) => {
             <iframe src={videoSrc} />
         </div>
         <div className="ui segment">
-            {/* <h4 className="ui header">{video.snipppet.title}</h4> */}
+            <h4 className="ui header">{video.snippet.title}</h4>
             <p>{video.snippet.description}</p>
         </div>
     </div>
